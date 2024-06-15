1822733ef92fc788017fe408800be679155889bd
Tyonakaisan <mikanno82@gmail.com>
2024-06-16 02:36:58 +0900

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     paperLibrary("com.google.inject", "guice", "7.0.0")
 }
 
-version = "1.0-SNAPSHOT"
+version = "1.1.0-SNAPSHOT"
 
 paper {
     val mainPackage = "github.tyonakaisan.sukesuke"
